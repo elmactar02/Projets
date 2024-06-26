d0c38cd6c49857a481487d0d657c5d02714839c7
elmactar02 <153730107+elmactar02@users.noreply.github.com>
2024-06-26 10:53:44 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+//Liste de mot utilisé pour le jeu
+
+let listMots = ["Cachalot","Pétunia","Serviette"];
+let listPhrases = ["Pas de panique !","La vie, l'univers et le reste","Merci pour le poisson"] ;
